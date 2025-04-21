cfec9e83a81f38b2108572e07a2a5cd52510b4a8
tonybierman <tonybierman@gmail.com>
2025-04-21 09:42:21 -0500

    AnalyticsReportPartial

---
+++ BiermanTech.CriticalDog/ViewModels/AnalyticsReportPartialViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BiermanTech.CriticalDog.Analytics;
+
+namespace BiermanTech.CriticalDog.ViewModels
+{
+    public class AnalyticsReportPartialViewModel
+    {
+        public ObservationChangeReport Report { get; set; } = new ObservationChangeReport();
+
+    }
+}
