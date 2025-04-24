d4ae8948ea9de8ace6253facfb3eabb7ad8ad2d5
tonybierman <tonybierman@gmail.com>
2025-04-24 08:59:32 -0500

    tabbed interface for subject details page

---
+++ BiermanTech.CriticalDog/ViewModels/DisciplineTabContentViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BiermanTech.CriticalDog.Data;
+
+namespace BiermanTech.CriticalDog.ViewModels
+{
+    public class DisciplineTabContentViewModel
+    {
+        public string DisciplineKey { get; set; }
+        public List<SubjectRecordViewModel> Records { get; set; }
+    }
+}
